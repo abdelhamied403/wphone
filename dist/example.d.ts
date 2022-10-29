48752578e7d021ad8dc2638a5a79791a9ff89d01
Pedro Sanders <psanders@fonoster.com>
2022-10-29 14:59:22 -0400

    chore(deps): upgrades sip.js dependency

--- dist/example.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const connectButton: HTMLButtonElement;
-export declare const disconnectButton: HTMLButtonElement;
-export declare const sendDtmf: HTMLButtonElement;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { CallRequest, MessageRequest, WPhoneConfig, InviterConfig } from "./types";
-import WPhone from "./wphone";
-export default WPhone;
-export { WPhoneConfig, CallRequest, MessageRequest, InviterConfig };
-export * from "./utils";
